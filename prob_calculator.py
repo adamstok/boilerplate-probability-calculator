9f63d2c3181b5c7a16dce795484a009225729258
adamstok <adamstokowski@outlook.com>
2020-12-22 18:51:47 +0100

    hat class

--- prob_calculator.py
+++ prob_calculator.py
@@ -4,7 +4,24 @@ import random
 
 
 class Hat:
-    pass
+    def __init__(self, **kwargs):
+        self.contents = []
+        for color in kwargs:
+            self.color = kwargs[color]
+            for _ in range(kwargs[color]):
+                self.contents.append(color)
+
+    def draw(self, numToDraw):
+        outp = []
+        if numToDraw > len(self.contents):
+            outp = self.contents[::]
+            self.contents = []
+            return outp
+        else:
+            for _ in range(numToDraw):
+                outp.append(self.contents.pop(
+                    random.randint(0, len(self.contents) - 1)))
+            return outp
 
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
